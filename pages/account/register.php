eccd621572dcf2b771a4cc80c23a6b807672e6d1
Well-Script <91045557+WellOfficial@users.noreply.github.com>
2022-12-13 16:53:36 +0700

    Added new files

---
+++ pages/account/register.php
@@ -0,0 +1,35 @@
+<link href="https://pro.fontawesome.com/releases/v5.15.0/css/all.css" rel="stylesheet">
+
+<section class="vh-100 gradient-custom">
+<div class="container py-5 h-100">
+<div class="row d-flex justify-content-center align-items-center h-100">
+<div class="col-12 col-md-8 col-lg-6 col-xl-7">
+<div class="card bg-white text-dark" style="border-radius: 4px;">
+<div class="card-body p-4 text-center">
+<div class="mb-md-0 pb-4">
+<h3 class="text-left"></i> AUTH | สมัครสมาชิก</h3>
+<h6 class="text-left" style="color:Gray;"><i class="fad fa-door-open"></i> สมัครสมาชิกเพื่อรับสิทธิ์ต่างๆภายในร้าน</h6>
+<div class="form-outline form-white mb-2 mt-3">
+<input id="username" type="text" class="form-control form-control-lg" placeholder="ชื่อบัญชีผู้ใช้งาน">
+<div class="form-outline form-white mb-2">
+</div>
+<input id="password" type="password" class="form-control form-control-lg" placeholder="รหัสผ่าน">
+</div>
+<div class="form-outline form-white mb-3">
+<input id="password_cm" type="password" class="form-control form-control-lg" placeholder="ยืนยันรหัสผ่าน">
+</div>
+<button class="btn btn-outline-dark btn-lg px-4" id="submit_register" type="button">สมัครสมาชิก <i class="fad fa-chevron-right"></i></button>
+<div class="d-flex justify-content-center text-center mt-4 pt-1">
+</div>
+</div>
+<div>
+<p class="mb-0">มีบัญชีอยู่แล้วหรอ? <a href="/login" class="text-dark fw-bold">เข้าสู่ระบบ</a>
+</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+<script src="../assets/js/register.js"></script>
